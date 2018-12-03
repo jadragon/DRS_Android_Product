49e39f27a45a8a31af5851129886461acd057a55
jadragon <h124008597@gmail.com>
2018-12-04 01:04:28 +0800

    全民代駕 v1.0.7

--- DesignatedDriving/app/src/main/java/tw/com/lccnet/app/designateddriving/CalculateActivity.java
+++ DesignatedDriving/app/src/main/java/tw/com/lccnet/app/designateddriving/CalculateActivity.java
@@ -1,32 +1,153 @@
 package tw.com.lccnet.app.designateddriving;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import tw.com.lccnet.app.designateddriving.API.Analyze.AnalyzeUtil;
+import tw.com.lccnet.app.designateddriving.API.CustomerApi;
+import tw.com.lccnet.app.designateddriving.Utils.AsyncTaskUtils;
+import tw.com.lccnet.app.designateddriving.Utils.IDataCallBack;
 
 public class CalculateActivity extends ToolbarActivity {
     private Spinner sp_time, sp_type;
-    private ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"07:00~23:59", "00:00~06:59"});
-    private ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"07:00~23:59", "00:00~06:59"});
-    private ArrayAdapter<String> arrayAdapter3 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"07:00~23:59", "00:00~06:59"});
-    private ArrayAdapter<String> arrayAdapter4 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"07:00~23:59", "00:00~06:59"});
+    private EditText calculate_edit1, calculate_edit2;
+    private Button calculate_btn;
+    private TextView pay, distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate);
         initToolbar("費用試算", true);
-        //   initSpinner();
+        initEditText();
+        initSpinner();
+        initButton();
 
     }
 
+    private void initButton() {
+        calculate_btn = findViewById(R.id.calculate_btn);
+        calculate_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sp_type.getSelectedItemPosition() < 2) {
+                    if (TextUtils.isEmpty(calculate_edit1.getText())) {
+                        calculate_edit1.setError("起始位置不能為空");
+                        return;
+                    } else if (TextUtils.isEmpty(calculate_edit2.getText())) {
+                        calculate_edit2.setError("終點位置不能為空");
+                        return;
+                    }
+                } else {
+                    if (TextUtils.isEmpty(calculate_edit1.getText())) {
+                        calculate_edit1.setError("時間不能為空");
+                        return;
+                    }
+                }
+                AsyncTaskUtils.doAsync(new IDataCallBack<JSONObject>() {
+                    @Override
+                    public JSONObject onTasking(Void... params) {
+                        switch (sp_type.getSelectedItemPosition()) {
+                            case 0:
+                                return CustomerApi.calculate1((sp_time.getSelectedItemPosition() + 1) + "", calculate_edit1.getText().toString(), calculate_edit2.getText().toString());
+                            case 1:
+                                return CustomerApi.calculate2(calculate_edit1.getText().toString(), calculate_edit2.getText().toString());
+                            case 2:
+                                return CustomerApi.calculate3((sp_time.getSelectedItemPosition() + 1) + "", calculate_edit1.getText().toString());
+                            default:
+                                return null;
+                        }
+                    }
+
+                    @Override
+                    public void onTaskAfter(JSONObject jsonObject) {
+                        if (jsonObject != null) {
+                            if (AnalyzeUtil.checkSuccess(jsonObject)) {
+                                try {
+                                    pay.setText(jsonObject.getJSONObject("Data").getString("pay"));
+                                } catch (JSONException e) {
+                                    pay.setText("");
+                                    e.printStackTrace();
+                                }
+                                try {
+                                    distance.setText(jsonObject.getJSONObject("Data").getString("distance"));
+                                } catch (JSONException e) {
+                                    distance.setText("");
+                                    e.printStackTrace();
+                                }
+                            } else {
+                                Toast.makeText(CalculateActivity.this, AnalyzeUtil.getMessage(jsonObject), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                    }
+                });
+
+            }
+
+        });
+    }
+
+    private void initEditText() {
+        pay = findViewById(R.id.pay);
+        distance = findViewById(R.id.distance);
+        calculate_edit1 = findViewById(R.id.calculate_edit1);
+        calculate_edit2 = findViewById(R.id.calculate_edit2);
+    }
+
     private void initSpinner() {
+        final ArrayAdapter arrayAdapter1 = new ArrayAdapter<>(this, R.layout.spinner_item, new String[]{"07:00~23:59", "00:00~06:59"});
+        final ArrayAdapter arrayAdapter2 = new ArrayAdapter<>(this, R.layout.spinner_item, new String[]{"00:00~06:59"});
+        ArrayAdapter arrayAdapter3 = new ArrayAdapter<>(this, R.layout.spinner_item, new String[]{"立即", "長途", "鐘點"});
         sp_time = findViewById(R.id.sp_time);
         sp_time.setAdapter(arrayAdapter1);
 
-
         sp_type = findViewById(R.id.sp_type);
-        sp_type.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"立即", "長途", "鐘點"}));
+        sp_type.setAdapter(arrayAdapter3);
+        sp_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        calculate_edit1.setInputType(InputType.TYPE_CLASS_TEXT);
+                        calculate_edit1.setHint("請輸入起始地址");
+                        calculate_edit2.setVisibility(View.VISIBLE);
+                        calculate_edit2.setHint("請輸入終點地址");
+                        sp_time.setAdapter(arrayAdapter1);
+                        break;
+                    case 1:
+                        calculate_edit1.setInputType(InputType.TYPE_CLASS_TEXT);
+                        calculate_edit1.setHint("請輸入起始地址");
+                        calculate_edit2.setVisibility(View.VISIBLE);
+                        calculate_edit2.setHint("請輸入終點地址");
+                        sp_time.setAdapter(arrayAdapter2);
+                        break;
+                    case 2:
+                        calculate_edit1.setInputType(InputType.TYPE_CLASS_NUMBER);
+                        calculate_edit1.setHint("請輸入預計時間(單位:分鐘)");
+                        calculate_edit2.setVisibility(View.INVISIBLE);
+                        sp_time.setAdapter(arrayAdapter1);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
 }
--- DesignatedDriving/app/src/main/java/tw/com/lccnet/app/designateddriving/OrdermealActivity.java
+++ DesignatedDriving/app/src/main/java/tw/com/lccnet/app/designateddriving/OrdermealActivity.java
@@ -1,15 +1,20 @@
 package tw.com.lccnet.app.designateddriving;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import tw.com.lccnet.app.designateddriving.API.CustomerApi;
+import tw.com.lccnet.app.designateddriving.Component.ResizableImageView;
 import tw.com.lccnet.app.designateddriving.Utils.AsyncTaskUtils;
 import tw.com.lccnet.app.designateddriving.Utils.IDataCallBack;
 
@@ -28,7 +33,30 @@ public class OrdermealActivity extends AppCompatActivity {
             @Override
             public void onTaskAfter(JSONObject jsonObject) {
                 try {
-                    ImageLoader.getInstance().displayImage(jsonObject.getString("Data"), (ImageView) findViewById(R.id.image));
+                    ImageLoader.getInstance().loadImage(jsonObject.getString("Data"), new ImageLoadingListener() {
+                        @Override
+                        public void onLoadingStarted(String s, View view) {
+
+                        }
+
+                        @Override
+                        public void onLoadingFailed(String s, View view, FailReason failReason) {
+
+                        }
+
+                        @Override
+                        public void onLoadingComplete(String s, View view, Bitmap bitmap) {
+                            ImageView imageView = findViewById(R.id.image);
+                            imageView.setImageBitmap(bitmap);
+
+                        }
+
+                        @Override
+                        public void onLoadingCancelled(String s, View view) {
+
+                        }
+                    });
+                    // ImageLoader.getInstance().displayImage(jsonObject.getString("Data"), );
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -36,6 +64,5 @@ public class OrdermealActivity extends AppCompatActivity {
         });
 
     }
-
 
 }
